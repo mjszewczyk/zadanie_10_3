f6d1285c1917c254e721a3a3847a159aa28070de
Magda <mjszewczyk91@gmail.com>
2018-03-10 17:18:39 +0100

    Add arrows and circles

--- js/scripts.js
+++ js/scripts.js
@@ -15,5 +15,29 @@
                 marginLeft: 0
             });
         }
-        setInterval(changeSlide, 3000);
+
+        function changeSlideLeft() {
+            carouselList.animate({
+                'marginLeft': 0
+            }, 500);
+        }
+        var myTimer = setInterval(changeSlide, 3000);
+        var rightArrow = $('.arrow-right');
+        rightArrow.click(function () {
+            clearInterval(myTimer);
+            myTimer = setInterval(changeSlide, 3000);
+            changeSlide();
+        });
+        var leftArrow = $('.arrow-left');
+        leftArrow.click(function () {
+            clearInterval(myTimer);
+            myTimer = setInterval(changeSlide, 3000);
+            var firstItem = carouselList.find("li:first");
+            var lastItem = carouselList.find("li:last");
+            firstItem.before(lastItem);
+            carouselList.css({
+                marginLeft: -500
+            });
+            changeSlideLeft();
+        });
     });
