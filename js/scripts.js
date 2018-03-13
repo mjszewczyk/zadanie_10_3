0a5d6d116dbdbca579decbe5798a7a8e0b4a71bf
Magda <mjszewczyk91@gmail.com>
2018-03-13 19:41:37 +0100

    Change style.css, index.html and scripts.js.
    In scripts.js add whole carousel logic.

--- js/scripts.js
+++ js/scripts.js
@@ -1,43 +1,86 @@
-    $(function () {
-        var carouselList = $("#carousel ul");
-
-        function changeSlide() {
-            carouselList.animate({
-                'marginLeft': -500
-            }, 500, moveFirstSlide);
-        }
-
-        function moveFirstSlide() {
-            var firstItem = carouselList.find("li:first");
-            var lastItem = carouselList.find("li:last");
-            lastItem.after(firstItem);
-            carouselList.css({
-                marginLeft: 0
-            });
-        }
-
-        function changeSlideLeft() {
-            carouselList.animate({
-                'marginLeft': 0
-            }, 500);
-        }
-        var myTimer = setInterval(changeSlide, 3000);
-        var rightArrow = $('.arrow-right');
-        rightArrow.click(function () {
-            clearInterval(myTimer);
-            myTimer = setInterval(changeSlide, 3000);
-            changeSlide();
+$(function () {
+    var carouselList = $("#carousel ul");
+    var circles = $(".circ");
+    // Resetowanie czasu animacji po kliknięciu strzałki lub kółka
+    function resetTimer() {
+        clearInterval(myTimer);
+        myTimer = setInterval(changeSlide, 3000);
+    }
+    // Zmiana slajdu w prawo
+    function changeSlide() {
+        carouselList.animate({
+            "marginLeft": -500
+        }, 500, moveFirstSlide);
+    }
+    // Funkcja przepinająca pierwszy slajd na ostatni
+    function moveFirstSlide() {
+        var firstItem = carouselList.find("li:first");
+        var lastItem = carouselList.find("li:last");
+        lastItem.after(firstItem);
+        carouselList.css({
+            marginLeft: 0
         });
-        var leftArrow = $('.arrow-left');
-        leftArrow.click(function () {
-            clearInterval(myTimer);
-            myTimer = setInterval(changeSlide, 3000);
-            var firstItem = carouselList.find("li:first");
-            var lastItem = carouselList.find("li:last");
-            firstItem.before(lastItem);
-            carouselList.css({
-                marginLeft: -500
-            });
-            changeSlideLeft();
-        });
+        checkCurrentCircle();
+    }
+    // Zmiana slajdu w lewo
+    function changeSlideLeft() {
+        carouselList.animate({
+            "marginLeft": 0
+        }, 500);
+        checkCurrentCircle();
+    }
+    // Zmiana slajdu powoduje zakolorowanie odpowiedniego kółka
+    function checkCurrentCircle() {
+        var firstImage = $(carouselList.find("li:first").find("img"));
+        circles.each(function (index, elem) {
+            var sufix = index + 1;
+            if (firstImage.attr("class") == ("img" + sufix)) {
+                $(elem).addClass("active");
+            }
+            else {
+                $(elem).removeClass("active");
+            }
+        })
+    };
+    // Obsługa kliknięcia w kółko
+    circles.on("click", function (e) {
+        if (!$(e.target).hasClass("active")) {
+            circles.each(function (index, elem) {
+                if (elem === e.target) {
+                    resetTimer();
+                    //                    console.log("znalazlem " + index);
+                    var sufix = index + 1;
+                    //wyszukuję element do wyświetlenia
+                    var image = carouselList.find(".img" + sufix);
+                    var item = image.parent();
+                    //                    console.log(item);
+                    var lastItem = carouselList.find("li:last");
+                    carouselList.find("li:first");
+                    var reversedItems = Array.prototype.reverse.call(item.prevAll());
+                    lastItem.after(reversedItems);
+                    checkCurrentCircle();
+                }
+            })
+        }
     });
+    // Timer (3 sekundowa animacja) 
+    var myTimer = setInterval(changeSlide, 3000);
+    // przejście slajdu w prawo po kliknięciu w strzałkę
+    var rightArrow = $(".arrow-right");
+    rightArrow.on("click", function () {
+        resetTimer();
+        changeSlide();
+    });
+    // przejście slajdu w lewo
+    var leftArrow = $(".arrow-left");
+    leftArrow.on("click", function () {
+        resetTimer();
+        var firstItem = carouselList.find("li:first");
+        var lastItem = carouselList.find("li:last");
+        firstItem.before(lastItem);
+        carouselList.css({
+            marginLeft: -500
+        });
+        changeSlideLeft();
+    });
+});
